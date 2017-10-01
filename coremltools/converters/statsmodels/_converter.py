9c0f54124afad5d9808ffd92a5db27971bde1496
masterswan <masterswanwhite@gmail.com>
2017-09-30 20:52:50 -0400

    Indentation fix

--- coremltools/converters/statsmodels/_converter.py
+++ coremltools/converters/statsmodels/_converter.py
@@ -4,7 +4,7 @@ Defines the primary function for converting statsmodels models.
 
 def convert(sm_obj, input_features = None,
         output_feature_names = None):
-	from ...models import MLModel
+    from ...models import MLModel
     from ._converter_internal import _convert_statsmodels_model
 
     spec = _convert_statsmodels_model(
