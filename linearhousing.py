6a7caf70b959c219cc22805f2af9caf9d6e978cd
masterswan <masterswanwhite@gmail.com>
2017-10-01 08:28:19 -0400

    A bit more cleanup to the demo script

--- linearhousing.py
+++ linearhousing.py
@@ -10,10 +10,13 @@ statsmodels_linear_model = sm.OLS(data["Price"], data[["Bedrooms", "Bathrooms", 
 fitted_statsmodel_model = statsmodels_linear_model.fit()
 
 # Convert fitted_statsmodel_model to .mlmodel
-coreml_model = coremltools.converters.statsmodels.convert(fitted_statsmodel_model, ["Bedrooms", "Bathrooms", "Size"], "Price")
+coreml_model = coremltools.converters.statsmodels\
+    .convert(fitted_statsmodel_model, ["Bedrooms", "Bathrooms", "Size"], "Price")
 
 # Print statsmodels prediction
-print "STATSMODELS PREDICTION (2 Bathrooms, 4 Bedrooms, 2000 SQ. Feet) \n" + str(fitted_statsmodel_model.predict([4, 2, 2000])[0]) + "\n"
+print "\nSTATSMODELS PREDICTION (2 Bathrooms, 4 Bedrooms, 2000 SQ. Feet) \n"\
+      + str(fitted_statsmodel_model.predict([4, 2, 2000])[0]) + "\n"
 
 # Print coreml_model prediction
-print "COREML MODEL PREDICTION (2 Bathrooms, 4 Bedrooms, 2000 SQ. Feet) \n" + str(coreml_model.predict({'Bedrooms': 4, 'Bathrooms': 2, 'Size': 2000})['Price'])
+print "COREML MODEL PREDICTION (2 Bathrooms, 4 Bedrooms, 2000 SQ. Feet) \n"\
+      + str(coreml_model.predict({'Bedrooms': 4, 'Bathrooms': 2, 'Size': 2000})['Price'])
