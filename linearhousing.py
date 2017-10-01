c559f805205ea22be1ea332770747ea97edaa3ed
masterswan <masterswanwhite@gmail.com>
2017-10-01 08:23:27 -0400

    Cleanup of linear housing prediction script

--- linearhousing.py
+++ linearhousing.py
@@ -1,29 +1,19 @@
-from sklearn.linear_model import LinearRegression
 import pandas as pd
 import coremltools
 import statsmodels.api as sm
 
-# Load data
+# Load data from file
 data = pd.read_csv('houses.csv')
 
-# Begin sklearn fitting
-#-----
-# Train a model
-"""
-model = LinearRegression()
-model.fit(data[["Bedrooms", "Bathrooms", "Size"]], data["Price"])
-coreml_model = coremltools.converters.sklearn.convert(model, ["Bedrooms", "Bathrooms", "Size"], "Price")
-print coreml_model.predict({'Bedrooms': 1.0, 'Bathrooms': 1.0, 'Size': 1240})
-"""
-
 # Begin statsmodels fitting
-#-----
-smodel = sm.OLS(data["Price"], data[["Bedrooms", "Bathrooms", "Size"]])
-result = smodel.fit()
-coreml_model = coremltools.converters.statsmodels.convert(result, ["Bedrooms", "Bathrooms", "Size"], "Price")
-print coreml_model.predict({'Bedrooms': 1.0, 'Bathrooms': 1.0, 'Size': 1240})
-coreml_model.save("linearhousingmodel.mlmodel")
-# print result.predict(['Bedrooms', 'Bathrooms', 'Size'], exog=[1.0, 1.0, 1240])
-print result.predict([4.0, 2.5, 2500])
-# print result.predict(exog={'Bedrooms': 1.0, 'Bathrooms': 1.0, 'Size': 1240})
-# print result.summary()
+statsmodels_linear_model = sm.OLS(data["Price"], data[["Bedrooms", "Bathrooms", "Size"]])
+fitted_statsmodel_model = statsmodels_linear_model.fit()
+
+# Convert fitted_statsmodel_model to .mlmodel
+coreml_model = coremltools.converters.statsmodels.convert(fitted_statsmodel_model, ["Bedrooms", "Bathrooms", "Size"], "Price")
+
+# Print statsmodels prediction
+print "STATSMODELS PREDICTION (2 Bathrooms, 4 Bedrooms, 2000 SQ. Feet) \n" + str(fitted_statsmodel_model.predict([4, 2, 2000])[0]) + "\n"
+
+# Print coreml_model prediction
+print "COREML MODEL PREDICTION (2 Bathrooms, 4 Bedrooms, 2000 SQ. Feet) \n" + str(coreml_model.predict({'Bedrooms': 4, 'Bathrooms': 2, 'Size': 2000})['Price'])
