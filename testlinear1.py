71d0aecd572335082b233c78a013a873d99a20ee
Ethan March <ethmarch@gmail.com>
2017-10-01 04:07:17 -0400

    generated .mlmodel

--- testlinear1.py
+++ testlinear1.py
@@ -10,8 +10,8 @@ logit_res = logit_mod.fit()
 #print logit_mod
 #print logit_res
 #print logit_res.model
-converted_model = coremltools.converters.statsmodels.convert(logit_res, ['x'], ['y'])
-#converted_model.save("testlinear1.mlmodel")
-prediction = converted_model.predict({'x' : 5})
-print prediction
+converted_model = coremltools.converters.statsmodels.convert(logit_res, ['x'], 'y')
+converted_model.save("testlinear1.mlmodel")
+#prediction = converted_model.predict({'x' : 5})
+#print prediction
 print converted_model
