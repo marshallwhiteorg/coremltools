c9055e5d12fe54804e4a028aab273a373d097aab
masterswan <masterswanwhite@gmail.com>
2017-10-01 01:07:21 -0400

    DS_Store in gitignore, more for the test scripts

--- testlinear1.py
+++ testlinear1.py
@@ -2,13 +2,16 @@ import statsmodels.api as sm
 import coremltools
 
 x = [1, 2, 3, 4, 5]
-y = [.1, .2, .3, .4, .5]
+y = [2, 4, 6, 8, 10]
 spector_data = sm.datasets.spector.load()
 spector_data.exog = sm.add_constant(spector_data.exog)
-logit_mod = sm.Logit(y, x)
+logit_mod = sm.OLS(y, x)
 logit_res = logit_mod.fit()
+#print logit_mod
+#print logit_res
+#print logit_res.model
 converted_model = coremltools.converters.statsmodels.convert(logit_res, ['x'], ['y'])
-converted_model.save("testlinear1.mlmodel")
+#converted_model.save("testlinear1.mlmodel")
 prediction = converted_model.predict({'x' : 5})
 print prediction
 print converted_model
